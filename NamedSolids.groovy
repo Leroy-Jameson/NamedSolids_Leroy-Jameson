bacdd6a039ce735a2d20353b765d7105dc061d64
jamesgevry <jamesgevry@localhost>
2024-07-13 15:53:27 -0400

    Adding ignore for /**.FCBak

--- NamedSolids.groovy
+++ NamedSolids.groovy
@@ -2,7 +2,8 @@ double size =20
 return [
 new Dodecahedron(size).toCSG().movex(size*0),
 new Icosahedron(size).toCSG().movex(size*2),
-new Octahedron(size).toCSG().movex(size*4),
-new Tetrahedron(size).toCSG().movex(size*6),
+new Octahedron(size).toCSG().movex(size*6),
+new Tetrahedron(size).toCSG().movex(size*8), 
+new Icosahedron(size).toCSG().movex(size*4),
 
 ]
